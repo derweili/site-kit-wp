ee62ac3642b9402911ea1850a8e030f1b7a9196d
Julian Weiland <julian.weiland@get10up.com>
2023-11-23 16:52:29 +0100

    Improve vrt test spec.

--- tests/playwright/vrt-tests.spec.js
+++ tests/playwright/vrt-tests.spec.js
@@ -17,11 +17,26 @@ test.describe.configure( { mode: 'parallel' } );
 scenarios.forEach( ( story ) => {
 	const [ , storyID ] = story.url.split( '?id=' );
 	test( `VRT ${ storyID }`, async ( { page } ) => {
+		const clickSelector = story.clickSelector || null;
+
 		//open local file in playwright
 		// cut story.url path on "="
 
 		global.console.log( 'open', `file://${ basePath }${ story.url }` );
+
 		await page.goto( `file://${ basePath }${ story.url }` );
+
+		// wait for fonts to load
+		await page.evaluate( () => document.fonts.ready );
+
+		if ( story.readySelector ) {
+			await page.waitForSelector( story.readySelector );
+		}
+
+		if ( clickSelector ) {
+			await page.click( clickSelector );
+			await page.waitForTimeout( story.postInteractionWait || 0 );
+		}
 
 		// await page.goto( `/iframe?id=${ story }` );
 		await expect( page ).toHaveScreenshot();
